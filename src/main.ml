b6bae4697eec9812f3465d7f71a868a80006306e
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-03-20 18:54:02 +0100

    Some progress.

--- src/main.ml
+++ src/main.ml
@@ -3,52 +3,46 @@ open Term
 open Entry
 open Parser
 
-let mk_entry md e =
+let handle_entry md e =
   match e with
   | Decl(lc,id,st,ty) ->
-    begin
-      match Env.declare lc id st ty with
-      | OK () -> ignore(Compile.compile_declaration (mk_name md id) ty)
-      | Err e -> Errors.fail_env_error e
-    end
+      begin
+        match Env.declare lc id st ty with
+        | OK () -> Compile.compile_declaration (mk_name md id) ty
+        | Err e -> Errors.fail_env_error e
+      end
   | Def(lc,id,opaque,Some ty,te) ->
-    begin
-      let define = if opaque then Env.define_op else Env.define in
-      match define lc id te (Some ty) with
-      | OK () -> ignore(Compile.compile_definition (mk_name md id) ty te)
-      | Err e -> Errors.fail_env_error e
-    end
-  | Def _ -> failwith "Definition without types are not supported"
-  | Rules(rs) -> failwith "Rules are not part of the sttforall logic"
-  | Name _ -> ()
-  | _ -> failwith "Commands are not supported"
+      begin
+        let define = if opaque then Env.define_op else Env.define in
+        match define lc id te (Some ty) with
+        | OK () -> Compile.compile_definition (mk_name md id) ty te
+        | Err e -> Errors.fail_env_error e
+      end
+  | Def _   -> failwith "Definition without types are not supported"
+  | Rules _ -> failwith "Rules are not part of the sttforall logic"
+  | _       -> failwith "Commands are not supported"
 
-let run_on_file export file =
-  let input = open_in file in
-  debug 1 "Processing file '%s'..." file;
+let run_on_file file =
   let md = Env.init file in
   Confluence.initialize ();
-  handle_channel md (mk_entry md) input;
-  Errors.success "File '%s' was successfully checked." file;
-  if export && not (Env.export ()) then
-    Errors.fail dloc "Fail to export module '%a'." pp_mident (Env.get_name ());
+  let input = open_in file in
+  let entries = Parser.parse_channel md input in
+  close_in input;
+  let ast = List.map (handle_entry md) entries in
+  if not (Env.export ()) then
+    Errors.fail dloc "Fail to export module '%a'." pp_mident md;
   Confluence.finalize ();
-  close_in input
-
+  Errors.success "File '%s' was successfully checked." file;
+  let file = (try Filename.chop_extension file with _ -> file) ^ ".stt" in
+  let oc = open_out file in
+  Printf.fprintf oc "%a%!" Print.print_ast ast;
+  close_out oc
 
 let _ =
-  let export = ref false in
-  let output = ref None in
   let options = Arg.align
-    [ ( "-e"
-      , Arg.Set export
-      , " Generates an object file (\".dko\")" )
-    ; ( "-I"
+    [ ( "-I"
       , Arg.String Basic.add_path
-      , " Add folder to Dedukti path" )
-    ; ( "-o"
-      , Arg.String (fun s -> output := Some(s))
-      , " Output file for the printer") ]
+      , " Add folder to Dedukti path" ) ]
   in
   let usage = "Usage: " ^ Sys.argv.(0) ^ " [OPTION]... [FILE]...\n" in
   let usage = usage ^ "Available options:" in
@@ -57,10 +51,11 @@ let _ =
     Arg.parse options (fun f -> files := f :: !files) usage;
     List.rev !files
   in
-  try List.iter (run_on_file !export) files with
+  try List.iter run_on_file files with
   | Parse_error(loc,msg) ->
       let (l,c) = of_loc loc in
       Printf.eprintf "Parse error at (%i,%i): %s\n" l c msg;
       exit 1
+  | Failure err          -> Printf.eprintf "ERROR %s.\n" err; exit 1
   | Sys_error err        -> Printf.eprintf "ERROR %s.\n" err; exit 1
   | Exit                 -> exit 3
