0df5ac2b5cbb33748fd0cf59157e4e1b9fd5a149
François Thiré <franth2@gmail.com>
2018-07-07 04:00:16 +0200

    stuff

--- src/export.ml
+++ src/export.ml
@@ -2,9 +2,10 @@ type system = [`Coq | `Matita | `Pvs | `OpenTheory | `Lean | `Sttfa]
 
 module type E =
 sig
-  val extension: string
-  val print_ast : Format.formatter -> string -> Ast.ast -> unit
-  val print_bdd : Ast.ast -> unit
+    val system    : system
+    val extension : string
+    val print_ast : Format.formatter -> string -> Ast.ast -> unit
+    val print_bdd : Ast.ast -> unit
 end
 (*
 module PVS : E =
@@ -16,6 +17,7 @@ end
 
 module PVS : E =
 struct
+  let system    = `Pvs
   let extension = "pvs"
   let print_ast = Pvs.print_ast
   let print_bdd = Pvs.print_bdd
@@ -23,21 +25,15 @@ end
 
 module COQ : E =
 struct
+  let system    = `Coq
   let extension = "v"
   let print_ast = Coq.print_ast
   let print_bdd = Coq.print_bdd
 end
 
-(*
 module MATITA : E =
 struct
-  let extension = "ma"
-  let print_ast = Matita.print_ast
-end
-*)
-
-module MATITA : E =
-struct
+  let system    = `Matita
   let extension = "ma"
   let print_ast = Matita.print_ast
   let print_bdd = Matita.print_bdd
@@ -45,6 +41,7 @@ end
 
 module OPENTHEORY : E =
 struct
+  let system    = `OpenTheory
   let extension = "art"
   let print_ast = Opentheory.print_ast
   let print_bdd = Opentheory.print_bdd
@@ -52,6 +49,7 @@ end
 
 module LEAN : E =
 struct
+  let system    = `Lean
   let extension = "lean"
   let print_ast = Lean.print_ast
   let print_bdd = Lean.print_bdd
@@ -59,11 +57,53 @@ end
 
 module STTFA : E =
 struct
-  let extension = "lean"
+  let system    = `Sttfa
+  let extension = "dk"
   let print_ast = Sttfa.print_ast
   let print_bdd = Sttfa.print_bdd
 end
 
+module type JSON =
+sig
+  val print_json : Ast.ast -> unit
+end
+
+module MAKE_JSON(M:E) : JSON =
+struct
+  let theory_id =
+    match M.system with
+    | `Coq -> 1
+    | `Matita -> 2
+    | `Pvs -> 3
+    | `OpenTheory -> 4
+    | `Lean -> 5
+    | `Sttfa -> 6
+
+  let string_of_theory =
+    match M.system with
+    | `Coq -> "coq"
+    | `Matita -> "matita"
+    | `Pvs -> "pvs"
+    | `OpenTheory -> "opentheory"
+    | `Lean -> "lean"
+    | `Sttfa -> "sttfa"
+
+  let empty = Bson.empty
+
+  let add_parameter k v cont =
+    Bson.add_element k v cont
+
+  let prefix = "json"
+  let file md id =
+    Format.asprintf "%s/%s_%a_%a" prefix string_of_theory Pp.print_mident md Pp.print_ident id
+
+  let keyword_of_item item = failwith "todo"
+  let statement_of_item item = failwith "todo"
+  let type_of_item item = failwith "todo"
+  let body_of_item item = failwith "todo"
+  let print_json ast =
+    Bson.to_simple_json
+end
 
 let of_system : system -> (module E) = fun sys ->
   match sys with
--- src/export.mli
+++ src/export.mli
@@ -1,10 +1,18 @@
-type system = [`Coq | `Matita | `Pvs | `OpenTheory | `Lean | `Sttfa ]
+type system = [`Coq | `Matita | `Pvs | `OpenTheory | `Lean | `Sttfa]
 
 module type E =
 sig
-  val extension: string
+  val sysmte    : system
+  val extension : string
   val print_ast : Format.formatter -> string -> Ast.ast -> unit
   val print_bdd : Ast.ast -> unit
 end
+
+module type JSON =
+sig
+  val print_json : Ast.ast -> unit
+end
+
+module MAKE_JSON : functor (THEORY:E) -> JSON
 
 val of_system : system -> (module E)
