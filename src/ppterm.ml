57fe00d3cae8cb2995c7da4f3a267e4fb0950b66
Gabriel Hondet <gabrielhondet@gmail.com>
2019-07-09 14:51:11 +0200

    more consistency

--- src/ppterm.ml
+++ src/ppterm.ml
@@ -1,21 +1,21 @@
 type id = string [@@deriving yojson]
 type qid = id list [@@deriving yojson]
-type db =
-  { index: int
-  ; db_args: ppterm list }
+type var =
+  { v_symb: id
+  ; v_args: ppterm list }
+  [@@deriving yojson]
+and const =
+  { c_symb: qid
+  ; c_args: ppterm list }
   [@@deriving yojson]
 and binder =
-  { symbol: id
+  { b_symb: id
   ; bound: id
   ; annotation: ppterm option
   ; body: ppterm }
   [@@deriving yojson]
-and const =
-  { c_id: qid
-  ; c_args: ppterm list }
-  [@@deriving yojson]
 and ppterm =
-  | DB of db
+  | Var of var
   | Binder of binder
   | Const of const
 [@@deriving yojson]
