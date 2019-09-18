eb377de546c3e4182090e83e49a69f77204ec299
gabrielhdt <gabrielhondet@gmail.com>
2019-09-18 16:31:02 +0200

    [+] Some documentation

--- src/export/json_types.ml
+++ src/export/json_types.ml
@@ -32,16 +32,27 @@ end
 
 type dependency = string list [@@deriving yojson]
 
+(** An item is more or less a Dedukti entry, with additional information. *)
 type item =
   { name : string
+  (** A fully qualified name, the representation of a Uri. *)
   ; taxonomy : Uri.taxon
   ; term : Ppterm.t option
+  (** Depends on the item taxonomy,
+      - axiom/constant: [None];
+      - definition: type annotation;
+      - theorem: type. *)
   ; body : Ppterm.t
+  (** Depends on the item taxonomy,
+      - axiom: type;
+      - definition/constant: definition of the term;
+      - theorem: proof. *)
   ; deps : dependency list
+  (** Direct dependencies of the item (no transitive closure). *)
   ; theory : string list
   ; exp : string list
-  (** Available systems. *) }
-  [@@deriving yojson]
+  (** Available export systems. *) }
+[@@deriving yojson]
 
 type export =
   { system : string
@@ -50,7 +61,7 @@ type export =
   (** Path of file. *)
   ; etype : string option
   (** A textual representation of the element in the foreign system *) }
-  [@@deriving yojson]
+[@@deriving yojson]
 
 type document = item list
 [@@deriving yojson]
