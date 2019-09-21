6c59f733411d43ba389911766cc003042a0a3cec
gabrielhdt <gabrielhondet@gmail.com>
2019-09-21 17:44:53 +0200

    [-] removed hol

--- src/hol/hol.mli
+++
@@ -1,8 +0,0 @@
-val hol_type : Basic.ident
-val hol_bool : Basic.ident
-val hol_arr : Basic.ident
-
-val hol_term : Basic.ident
-val hol_eq : Basic.ident
-
-val is_hol_const : Basic.ident  -> Term.term -> bool
