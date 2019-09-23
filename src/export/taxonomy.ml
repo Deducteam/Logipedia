34d03be6059ee9f8a48f2144ed71790f95184734
gabrielhdt <gabrielhondet@gmail.com>
2019-09-23 15:56:50 +0200

    [~] documentation, cleaning

--- src/export/taxonomy.ml
+++ src/export/taxonomy.ml
@@ -7,18 +7,19 @@ module T = Term
 module U = Uri
 module Jt = Json_types
 
+exception IllTaxon
+(** Exception raised when reading an ill formed taxon. *)
+
 (** Specification of a taxon: conversion from Dedukti term. *)
 module type TaxonSpec = sig
   type t
 
   val taxons : t B.NameHashtbl.t
+  (** Contains all taxons of file and dependencies.  Kind of
+      memoization. *)
 
   val theory : string
   (** Name of the theory. *)
-
-  val default : t
-  (** A default label if no other is available. *)
-  (* Should be removed: all items ought to have a taxon. *)
 
   val of_def : T.term -> t
   (** [of_def t] returns a taxon of a term [t] given that [t] comes
@@ -33,7 +34,9 @@ module type TaxonSpec = sig
       of taxon [tx]. *)
 
   val of_string : string -> t
-  (** [of_string s] converts string [s] to a taxon. *)
+  (** [of_string s] converts string [s] to a taxon.
+
+      @raise IllTaxon if the string is not a taxon. *)
 end
 
 module Sttfa : TaxonSpec =
@@ -43,9 +46,6 @@ struct
     | TxDef (** Definition *)
     | TxCst (** Constant *)
     | TxThm (** Theorem *)
-    | TxNa  (** Not available *)
-
-  let default = TxNa
 
   let taxons : t B.NameHashtbl.t = B.NameHashtbl.create 0
 
@@ -75,7 +75,6 @@ struct
     | TxDef -> if short then "def" else "definition"
     | TxCst -> if short then "cst" else "constant"
     | TxThm -> if short then "thm" else "theorem"
-    | TxNa  -> if short then "na"  else "N/A"
 
   let of_string s =
     if s = "axiom" || s = "axm" then
@@ -87,7 +86,7 @@ struct
     else if s = "theorem" || s = "thm" then
       TxThm
     else
-      TxNa
+      raise IllTaxon
 end
 
 let taxonomise : B.mident -> E.entry list -> unit =
@@ -106,10 +105,7 @@ let taxonomise : B.mident -> E.entry list -> unit =
   List.iter f es
 
 (** [tax_find_or_imp k txs] tries to find key [k] in taxons map [txs].
-    If it fails, it tries to load the json where [k] is defined.
-    [k] is [(md, id)] where [md] and [id] are strings coming from
-    [mident] and [ident].
-    TODO use mident and ident rather than string string *)
+    If it fails, it tries to load the json where [k] is defined. *)
 let tax_find_or_parse : B.NameHashtbl.key -> Sttfa.t = fun key ->
   try Basic.NameHashtbl.find Sttfa.taxons key
   with Not_found ->
