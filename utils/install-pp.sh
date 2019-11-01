787fcfcd862f5e46af244e7401e0ecfa48091306
gabrielhdt <gabrielhondet@gmail.com>
2019-11-01 15:36:28 +0100

    [*] less verbose

--- utils/install-pp.sh
+++ utils/install-pp.sh
@@ -25,7 +25,7 @@ guile -c '(use-modules (json))'
 if [[ "$?" -ne 0 ]]
 then
     echo "Guile json required"
-    read -p "Install guile-json? [y/N]" answer
+    read -p "Install guile-json (from sources)? [y/N]" answer
     if [[ ! "$answer" -eq "y" ]]
     then
         exit 1
@@ -39,10 +39,10 @@ echo "Installing guile json..."
 tmpd="$(mktemp -d)"
 repo="https://git.savannah.nongnu.org/git/guile-json.git"
 cd "$tmpd"
-git clone "$repo" --branch 3.1.0 --quiet .
-autoreconf -if
+git clone "$repo" --branch 3.1.0 --quiet . > /dev/null
+autoreconf -if > /dev/null
 # FIXME get prefix from guile %site-dir?
-./configure --prefix=/usr
+./configure --prefix=/usr > /dev/null
 make
 sudo make install
 cd -
@@ -53,7 +53,7 @@ echo "Installing the pretty printer"
 tmpd="$(mktemp -d)"
 repo="https://github.com/gabrielhdt/logippedia.git"
 cd "$tmpd"
-git clone "$repo" --quiet .
+git clone "$repo" --quiet . > /dev/null
 cd 'scheme'
 sudo make install
 cd "$_wd"
@@ -64,4 +64,7 @@ then
     echo "Installation error"
     echo "You might want to check your paths"
     exit 1
+else
+    echo "Installation succeeded: $(which ${_logipp})"
+    exit 0
 fi
