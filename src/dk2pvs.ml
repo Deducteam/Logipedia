f2fa0ff22895bf3e4870ababa6e2b153a84d1957
gabrielhdt <gabrielhondet@gmail.com>
2019-11-20 15:52:45 +0100

    [-] removing dead code

--- src/dk2pvs.ml
+++ src/dk2pvs.ml
@@ -1,19 +1,12 @@
+(** PVS exporter module. *)
 module B = Kernel.Basic
 module E = Export
-module P = Parsing.Parser
-module S = Systems
-
-module Denv = Api.Env.Default
-module Derr = Api.Errors.Make(Denv)
 
 (** File into which exported file are written. *)
 let output_file = ref None
 
 (** Input dedukti files. *)
 let infile : string ref = ref ""
-
-(** The middleware used. *)
-let middleware : string ref = ref ""
 
 (** Options list, redefined according to first argument. *)
 let options : (string * Arg.spec * string) list =
