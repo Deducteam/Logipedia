8df71252e3fdc71936441dfc8f852e198c4bcf66
gabrielhdt <gabrielhondet@gmail.com>
2019-12-03 11:32:08 +0100

    [~] simplified logipedia.ml

--- src/logipedia.ml
+++ src/logipedia.ml
@@ -1,5 +1,6 @@
 module B = Kernel.Basic
 module D = Deps
+module E = Export
 module P = Parsing.Parser
 module S = Systems
 
@@ -53,41 +54,6 @@ let anon arg =
       let msg = Format.sprintf "Can't export to %s: system not supported" s in
       raise (Arg.Bad msg)
 
-let export_file ast system =
-  let (module M:Export.E) = Export.of_system system in
-  let (fmt, file) =
-    match !output_file with
-    | None    -> Format.std_formatter, None
-    | Some(f) ->
-      let file = open_out f in
-      (Format.formatter_of_out_channel file, Some(file))
-  in
-  M.print_ast fmt ast;
-  match file with
-  | None    -> ()
-  | Some(f) -> close_out f
-
-
-(* Compute an STTforall ast from Dedukti entries *)
-let mk_ast md entries =
-  let items = List.map (Compile.compile_entry md) entries in
-  let fold_entry_dep dep e = D.QSet.union dep
-      (Deps.dep_of_entry [Sttfadk.sttfa_module;md] e) in
-  let dep = List.fold_left fold_entry_dep D.QSet.empty entries in
-  { Ast.md = B.string_of_mident md; Ast.dep; items }
-
-(* Export the file for the chosen system. *)
-let export_system syst file =
-  let md = Denv.init file in
-  let input = open_in file in
-  let entries = P.Parse_channel.parse md input in
-  close_in input;
-  begin
-    let sttfa_ast = mk_ast md entries in
-    let (module M:Export.E) = Export.of_system syst in
-    export_file sttfa_ast syst;
-  end
-
 let _ =
   let available_sys = List.map fst S.sys_spec |> String.concat ", " in
   let usage = Format.sprintf
@@ -102,8 +68,18 @@ Available options for the selected mode:"
     match !export_mode with
     | None    -> raise @@ Arg.Bad "Missing export"
     | Some(s) ->
+      let outfmt, ochan =
+        match !output_file with
+        | None    -> Format.std_formatter, None
+        | Some(f) ->
+          let ochan = open_out f in
+          Format.formatter_of_out_channel ochan, Some(ochan)
+      in
       if !infile = "" then raise (Arg.Bad "Input file required");
-      export_system s !infile
+      E.export_system (E.of_system s) !infile outfmt;
+      match ochan with
+      | None     -> ()
+      | Some(oc) -> close_out oc
   with
   | Arg.Bad(s) ->
     Format.printf "%s\n" s;
