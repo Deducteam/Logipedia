664881d29890ad1158e1b9530bcc4419503e4000
Gabriel Hondet <gabrielhondet@gmail.com>
2019-12-12 09:41:23 +0100

    reworked a bit

--- src/make_json.ml
+++ src/make_json.ml
@@ -8,7 +8,41 @@ module Denv = Api.Env.Default
 let theory = "dummy"
 let theory_md = K.Basic.mk_mident theory
 
+let get_mod_deps : K.Basic.mident -> K.Basic.mident list = fun _ -> assert false
+
 module JsExp = Json.Compile.Make(Middleware.Sttfa)
+
+(** {1 General definitions} *)
+
+type key =
+  | JsMd of K.Basic.mident
+  | DkMd of K.Basic.mident
+
+(** {1 GNU Make like behaviour} *)
+(** Old style make rules. *)
+type ('k, 'v) rulem = { m_creates : 'k
+                      ; m_depends : 'k list
+                      ; m_action : 'v list -> 'v }
+
+let rec buildm : ('k, 'v) rulem list -> 'k -> 'v = fun rules target ->
+  let rule = List.find (fun r -> r.m_creates = target) rules in
+  rule.m_action (List.map (buildm rules) rule.m_depends)
+
+let make_doc_rulem : string -> (key, Json_types.document) rulem =
+  fun file ->
+  let md = Denv.init file in
+  let m_action _ =
+    (* Argument discarded as we previously built jsons in
+       [doc_of_entries]. Memoization can be done at the level of make,
+       and not [doc_of_entries]. *)
+    let input = open_in file in
+    let entries = Parsing.Parser.Parse_channel.parse md input in
+    JsExp.doc_of_entries md entries
+  in
+  let m_depends = List.map (fun x -> DkMd(x)) (get_mod_deps md) in
+  { m_creates=JsMd(md); m_depends; m_action}
+
+(** {1 Shake like behaviour} *)
 
 type ('k, 'v) action =
   | Finished of 'v
@@ -16,6 +50,7 @@ type ('k, 'v) action =
   (** A dependence on a key along with the way to use the value from
       this dependency. *)
 
+(** Dynamic rules. *)
 type ('k, 'v) rule = 'k * ('k, 'v) action
 
 let rec build : ('k, 'v) rule list -> 'k -> 'v = fun rules target ->
@@ -26,32 +61,13 @@ let rec build : ('k, 'v) rule list -> 'k -> 'v = fun rules target ->
   let action = snd (List.find (fun r -> (fst r) = target) rules) in
   run action
 
-let get_mod_deps : K.Basic.mident -> K.Basic.mident list = fun _ -> assert false
-
-type key =
-  | JsMd of K.Basic.mident
-  | DkMd of K.Basic.mident
-
-(** Rule to build the theory. *)
-let theory_rule : (key, Json_types.document) rule =
-  let input = open_in theory in
-  let md = Denv.init theory in
-  let entries = Parsing.Parser.Parse_channel.parse md input in
-  DkMd(theory_md), Finished(JsExp.doc_of_entries md entries)
-
-(** [compile_dkmod md] returns the action to compile the module [md]
-    including the dependency on the theory file. *)
-let compile_dkmod : string -> ('k, 'v) action = fun file ->
-  Depends(DkMd(theory_md), fun _ ->
-      let input = open_in file in
-      let md = Denv.init file in
-      let entries = Parsing.Parser.Parse_channel.parse md input in
-      Finished(JsExp.doc_of_entries md entries))
-
-let rule_of_dk_mod : string -> (key, Json_types.document) rule list =
-  fun file ->
+let make_doc_rule : string -> (key, Json_types.document) rule = fun file ->
   let md = Denv.init file in
-  [ ( JsMd(md)
-    , Depends(DkMd(md), fun v -> Finished(v)) )
-  ; ( DkMd(md)
-    , compile_dkmod file ) ]
+  let action =
+    Depends(DkMd(md), fun _ ->
+        let input = open_in file in
+        let entries = Parsing.Parser.Parse_channel.parse md input in
+        Depends(DkMd(theory_md), fun _ ->
+            Finished(JsExp.doc_of_entries md entries)))
+  in
+  JsMd(md), action
