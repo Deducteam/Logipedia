aefa06a96312b2bdd5dde6d9ad735ba25c92fb83
Gabriel Hondet <gabrielhondet@gmail.com>
2020-01-21 08:07:18 +0100

    shell script rather that makefile

---
+++ export.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+usage="Usage: $0 -e EXP -p PKG -t THY [-m MID]"
+while getopts 'e:p:t:d:m:h' arg
+do
+    case "$arg" in
+        m) mid="$OPTARG" ;;
+        e) exp="$OPTARG" ;;
+        t) thy="$OPTARG" ;;
+        p) pkg="$OPTARG" ;;
+        h) echo "$usage"
+           exit 0
+           ;;
+    esac
+done
+
+set -x
+
+if [[ -z ${exp:-''} ]]
+then
+    echo 'Export mode not specified'
+    echo "$usage"
+    exit 1
+fi
+
+# Download library
+cd "import/dedukti"
+if [[ ! -d "$thy" ]]
+then
+    curl "http://www.lsv.fr/~hondet/logipedia/${thy}.tar.bz2" | tar xj
+fi
+cd -
+
+out="export/${exp}"
+thdir="theories/${thy}"
+srcdir="import/dedukti/${thy}/${pkg}"
+
+if [[ "$exp" == "json" ]]
+then
+    middleware=${mid:-"$thy"}
+    ./dk2json -m "$middleware" -o "$out" -J "$out"\
+              -I "$thdir" -I "$srcdir" -d "$srcdir"
+else
+    ./logipedia "$exp" -I "$thdir" -I "$srcdir" -o "$out" -d "$srcdir"
+fi
