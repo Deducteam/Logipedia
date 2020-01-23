cce23817ad8f3fdfdc6b5028fa8ced177b66cb5b
Gaspard Ferey <gaspard.ferey@inria.fr>
2020-01-23 09:02:06 +0100

    WIP

--- src/cupicef/latex.ml
+++ src/cupicef/latex.ml
@@ -31,7 +31,10 @@ let rec pp_term fmt = function
   | App (f,a,args)     -> pp_list "\\ " pp_term_wp fmt (f::a::args)
   | Lam (_,x,None  ,t) -> fprintf fmt "\\lambda %a.%a" pp_ident x pp_term t
   | Lam (_,x,Some a,t) -> fprintf fmt "\\lambda %a:%a.%a" pp_ident x pp_term_wp a pp_term t
-  | Pi  (_,x,a,b)      -> fprintf fmt "\\Pi %a:%a.%a"     pp_ident x pp_term_wp a pp_term b
+  | Pi  (_,x,a,b)      ->
+    if Kernel.Subst.occurs 0 b
+    then fprintf fmt "\\Pi %a:%a.%a" pp_ident x pp_term_wp a pp_term b
+    else fprintf fmt "%a \rightarrow %a" pp_term_wp a pp_term b
 
 and pp_term_wp fmt = function
   | Kind | Type _ | DB _ | Const _ as t -> pp_term fmt t
@@ -50,8 +53,8 @@ and pp_const fmt i args = match i, args with
   | "Rule" , [s1;s2;s3] -> fprintf fmt "\\mathcal{A}_{%a, %a, %a}"
                              pp_term s1 pp_term s2 pp_term s3
   | "Cumul", [s1;s2]    -> fprintf fmt "\\mathcal{C}_{%a, %a}" pp_term s1 pp_term s2
-  | "Eq", [s1;s2]       -> fprintf fmt "%a = %a" pp_term s1 pp_term s2
-  | "and", [c1;c2]      -> fprintf fmt "%a \\wedge %a" pp_term c1 pp_term c2
+  | "Eq"   , [s1;s2]    -> fprintf fmt "%a = %a" pp_term s1 pp_term s2
+  | "and"  , [c1;c2]    -> fprintf fmt "%a \\wedge %a" pp_term c1 pp_term c2
 
   | "univ" , [u;_;_]    -> pp_term fmt u
   | "prod" , [_;_;_;_;a;Lam (_,x,_,b)]
