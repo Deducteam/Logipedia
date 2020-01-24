c6fc3c0b9dbf028ea775c8e8e13e4c9782006501
Gaspard Ferey <gaspard.ferey@inria.fr>
2020-01-24 14:12:42 +0100

    Extra latex printing.

--- src/cupicef/latex.ml
+++ src/cupicef/latex.ml
@@ -11,19 +11,23 @@ let escape_underscore =
   fun s -> Str.global_replace regexp "\\_" s
 
 let pp_ident fmt id = fprintf fmt "%s"
-    (escape_underscore (Kernel.Basic.string_of_ident id))
+    (escape_underscore (string_of_ident id))
 
 let pp_mident fmt id = fprintf fmt "%s"
-    (escape_underscore (Kernel.Basic.string_of_mident id))
+    (escape_underscore (string_of_mident id))
 
 let pp_name fmt name = fprintf fmt "%a.%a" pp_mident (md name) pp_ident (id name)
 
-let rec pp_term fmt = function
-  | Const (_,name) when mident_eq (md name) enc_md ->
-    pp_const fmt (string_of_ident (id name)) []
+let enc_ident name = mident_eq (md name) enc_md
 
-  | App (Const (_,name),a,args) when mident_eq (md name) enc_md ->
-    pp_const fmt (string_of_ident (id name)) (a::args)
+let enc_ident_eq name s = mident_eq (md name) enc_md && string_of_ident (id name) = s
+
+let rec pp_term fmt = function
+  | Const (_,name) when enc_ident name ->
+    pp_const fmt (string_of_ident (id name), [])
+
+  | App (Const (_,name),a,args) when enc_ident name ->
+    pp_const fmt (string_of_ident (id name), a::args)
 
   | Kind -> assert false
   | Type _             -> fprintf fmt "\\mathbb{T}"
@@ -39,9 +43,29 @@ let rec pp_term fmt = function
 
 and pp_term_wp fmt = function
   | Kind | Type _ | DB _ | Const _ as t -> pp_term fmt t
-  | t                                   -> fprintf fmt "(%a)" pp_term t
+  | App (Const (_,name),a,args) when enc_ident name ->
+    pp_const_wp fmt (string_of_ident (id name), a::args)
+  | t -> fprintf fmt "(%a)" pp_term t
 
-and pp_const fmt i args = match i, args with
+and pp_const_wp fmt c = match c with
+  (* Special cases, parentheses go on subterm (if necessary) *)
+  | "Term" , [_; a]     -> pp_term_wp fmt a
+  | "Univ" , [u]        -> pp_term_wp fmt u
+  | "univ" , [u;_;_]    -> pp_term_wp fmt u
+  | "cast",[_;_;_;_;_;t]-> pp_term_wp fmt t
+
+  (* Parentheses non necessary *)
+  | "eps"  , [_]
+  | "Axiom", [_;_]
+  | "Rule" , [_;_;_]
+  | "Cumul", [_;_]
+  | "type" , [_]
+  | "max", [_;_] -> pp_const fmt c
+
+  (* Adding parentheses *)
+  | c                   -> fprintf fmt "(%a)" pp_const c
+
+and pp_const fmt = function
   | "Sort" , []         -> fprintf fmt "\\mathbb{S}"
   | "sup"  , [s1; s2]   -> fprintf fmt "\\textbf{sup}(%a,%a)" pp_term s1 pp_term s2
   | "Univ" , [u]        -> pp_term fmt u
@@ -56,15 +80,32 @@ and pp_const fmt i args = match i, args with
   | "Cumul", [s1;s2]    -> fprintf fmt "\\mathcal{C}_{%a, %a}" pp_term s1 pp_term s2
   | "Eq"   , [s1;s2]    -> fprintf fmt "%a = %a" pp_term s1 pp_term s2
   | "and"  , [c1;c2]    -> fprintf fmt "%a \\wedge %a" pp_term c1 pp_term c2
-
   | "univ" , [u;_;_]    -> pp_term fmt u
   | "prod" , [_;_;_;_;a;Lam (_,x,_,b)]
-                        -> fprintf fmt "\\Pi %a:%a.%a"     pp_ident x pp_term_wp a pp_term b
-  | "cast" , [_;_;_;_;_;t] -> pp_term fmt t
-
+                        -> fprintf fmt "\\forall %a:%a, %a" pp_ident x pp_term_wp a pp_term b
+  | "SubType",[_;_;a;b] -> fprintf fmt "%a \\preceq %a}" pp_term a pp_term b
+  | "cast",[_;_;_;_;_;t]-> pp_term fmt t
+  | "Nat", []           -> fprintf fmt "\\mathcal{L}"
+  | "z", []             -> fprintf fmt "\\text{Set}"
+  | "s", [l]            -> pp_lvl 0 fmt l
+  | "prop", []          -> fprintf fmt "\\text{Prop}"
+  | "type" , [Const (_,name)] when enc_ident_eq name "z" -> fprintf fmt "\\text{Set}"
   | "type" , [s]        -> fprintf fmt "\\text{Type}_{%a}" pp_term s
+  | "set", []           -> fprintf fmt "\\text{Set}"
+  | "type0", []         -> fprintf fmt "\\text{Type}_0"
+  | "max", [l1;l2]      -> fprintf fmt "\\text{max}(%a,%a)" pp_term l1 pp_term l2
+
   | c, []               -> fprintf fmt "\\textbf{%s}" c
   | c, l                -> fprintf fmt "\\textbf{%s}\\ %a" c (pp_list "\\ " pp_term_wp) l
+
+and pp_lvl n fmt = function
+  | Const (_,name)
+       when enc_ident_eq name "z" -> fprintf fmt "%i" n
+  | App (Const (_,name),i,[])
+       when enc_ident_eq name "s" -> pp_lvl (n+1) fmt i
+  | c -> fprintf fmt "(%i + %a)" n pp_term c
+
+
 
 let pp_entry fmt = function
   | Decl  (_,_,_, ty)
